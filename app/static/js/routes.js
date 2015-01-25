7729180a97bdb194b4ba2669fa870ccbff741658
David <dkdewitt@gmail.com>
2015-01-25 15:42:50 -0700

    first commit

---
+++ app/static/js/routes.js
@@ -0,0 +1,33 @@
+angular.module('app.routes', ["ngRoute",'ui.bootstrap']);
+
+
+.config(function($routeProvider,$locationProvider) {
+
+
+    $locationProvider.html5Mode(true);
+    $routeProvider
+
+    .when('/register', {
+        templateUrl: '/static/templates/user_templates/register.html',
+        //controller: 'groupsController'
+    })
+
+    .when('/login', {
+        templateUrl: '/static/templates/user_templates/login.html',
+        //controller: 'groupsController'
+    })
+
+
+
+    .when('/logout', {template: ' ', controller: 'LogoutController',})  
+    .when('/', {
+            templateUrl: '/static/templates/home.html',
+           
+
+    })
+
+    .otherwise({
+       redirectTo: '/',  
+        });
+    
+    });
